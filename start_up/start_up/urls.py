b6e7c69eacd4f6ba77fcedbc84904a70c0f21efc
noorshanu <51040978+noorshanu@users.noreply.github.com>
2021-02-28 05:05:22 +0530

    update

--- start_up/start_up/urls.py
+++ start_up/start_up/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("it_startup.urls"))
+    path('', include("it_startup.urls"))
 ]
